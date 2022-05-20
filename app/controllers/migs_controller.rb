f674afdb88d498bb84e331b2aef3a7fce4d85ddb
DeeGeeWest <DeeGeeWest@gmail.com>
2022-05-20 17:26:27 -0400

    changed rout to only north carolina

--- app/controllers/migs_controller.rb
+++ app/controllers/migs_controller.rb
@@ -1,8 +1,8 @@
 class MigsController < ApplicationController
   def index
     
-    response = HTTP.headers("X-eBirdApiToken" => Rails.application.credentials.bird_api[:api_key]).get("https://api.ebird.org/v2/data/obs/US/recent/notable")
-   
+    response = HTTP.headers("X-eBirdApiToken" => Rails.application.credentials.bird_api[:api_key]).get("https://api.ebird.org/v2/data/obs/US-NC/recent/notable")
+   ##           US or US-State abrvieation
     render json: response.parse(:json)
    
   end
