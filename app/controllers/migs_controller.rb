94e03fc76dfb0c0a115e7ef58d11bf666eb9188b
DeeGeeWest <DeeGeeWest@gmail.com>
2022-05-27 16:04:38 -0400

    switched back to TN for personal use

--- app/controllers/migs_controller.rb
+++ app/controllers/migs_controller.rb
@@ -1,7 +1,7 @@
 class MigsController < ApplicationController
   def index
     
-    response = HTTP.headers("X-eBirdApiToken" => Rails.application.credentials.bird_api[:api_key]).get("https://api.ebird.org/v2/data/obs/US-NC/recent/notable")
+    response = HTTP.headers("X-eBirdApiToken" => Rails.application.credentials.bird_api[:api_key]).get("https://api.ebird.org/v2/data/obs/US-TN/recent/notable")
    ##           US or US-State abrvieation
     render json: response.parse(:json)
    
